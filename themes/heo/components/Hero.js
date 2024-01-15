1b52a9ec1b9f6699b0fb423e9fbddbec0d9af4b2
Phillweston <2436559745@qq.com>
2024-01-15 10:01:34 +0800

    Change button name in English

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -101,7 +101,7 @@ function Banner(props) {
         }
       >
         <div className="ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in">
-          <div className="text-7xl text-white font-extrabold">随便逛逛</div>
+          <div className="text-7xl text-white font-extrabold">Explore</div>
           <div className="-ml-3 text-gray-300">
             <ArrowSmallRight className={'w-24 h-24 stroke-2'} />
           </div>
@@ -243,9 +243,9 @@ function TopGroup(props) {
                 <div className="group-hover:text-indigo-600 dark:group-hover:text-yellow-600 line-clamp-2 overflow-hidden m-2 font-semibold">
                   {p?.title}
                 </div>
-                {/* hover 悬浮的 ‘荐’ 字 */}
+                {/* hover 悬浮的 ‘Recommendation’ 字 */}
                 <div className="opacity-0 group-hover:opacity-100 -translate-x-4 group-hover:translate-x-0 duration-200 transition-all absolute -top-2 -left-2 bg-indigo-600 dark:bg-yellow-600  text-white rounded-xl overflow-hidden pr-2 pb-2 pl-4 pt-4 text-xs">
-                  荐
+                  Recommendation
                 </div>
               </div>
             </Link>
@@ -367,7 +367,7 @@ function TodayCard({ cRef, siteInfo }) {
               className={'w-6 h-6 mr-2 bg-white rounded-full stroke-indigo-400'}
             />
             <div id="more" className="select-none">
-              更多推荐
+              More Recommendations
             </div>
           </div>
         </div>
